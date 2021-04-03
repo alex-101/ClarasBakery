defcb1128cb7b9aab24572e3bc57cc9277e30dc7
alex-101 <68401423+alex-101@users.noreply.github.com>
2021-04-03 20:56:25 +0100

    Add files via upload

---
+++ database.js
@@ -0,0 +1,21 @@
+var menu = [
+///
+{
+	name: "Tea",
+	description: "Enjoy a nice hot cuppa.",
+	price: "10P"
+},
+
+{
+	name: "Coffee",
+	description: "If you don't like tea, maybe try this?",
+	price: "10P"
+},
+
+{
+	name: "Cookies",
+	description: "Yum Yum!",
+	price: "10P"
+}
+///
+]
