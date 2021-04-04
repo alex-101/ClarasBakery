602af357188dd1da2878b1fd15b57479f7e00954
SetAsync <68401423+alex-101@users.noreply.github.com>
2021-04-04 18:18:37 +0100

    Add files via upload

--- database.js
+++ database.js
@@ -3,19 +3,29 @@ var menu = [
 {
 	name: "Tea",
 	description: "Enjoy a nice hot cuppa.",
-	price: "10P"
+	price: "10P",
+	type: "drinks"
 },
 
 {
 	name: "Coffee",
 	description: "If you don't like tea, maybe try this?",
-	price: "10P"
+	price: "10P",
+	type: "drinks"
 },
 
 {
-	name: "Cookies",
+	name: "Sticks",
 	description: "Yum Yum!",
-	price: "10P"
+	price: "10P",
+	type: "shortbread"
+},
+
+{
+	name: "Stars",
+	description: "N/A",
+	price: "10P",
+	type: "shortbread"
 }
 ///
 ]
